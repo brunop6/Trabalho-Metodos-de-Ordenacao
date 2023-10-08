0113ba55cd18192a2531e4ece25e3651864d0312
brunop6 <peresbruno6@gmail.com>
2023-10-08 16:18:16 -0300

    Implementação Bubble Sort

--- app.cpp
+++ app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
@@ -36,17 +37,120 @@ bool createArrays() {
     file.close();
 
     system("cls");
-    cout << "Vetores criados!\n" << endl;
     return true;
   }else {
     return false;
   }
 }
 
+/*
+Realizando a passagem por valor e não por referência
+para manter desordenado para os próximos algoritmos.
+Obs.: Os valores não serão retornados pois nos interessa apenas
+os tempos gastos em ordenação;
+*/
+void bubbleSort(string vet[], int n) {
+  string aux;
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n-1-i; j++){
+      if(vet[j+1] < vet[j]){
+        aux = vet[j];
+        vet[j] = vet[j+1];
+        vet[j+1] = aux;
+      }
+    }
+  }
+
+  //for(int i = 0; i < n; i++) cout << vet[i] << endl;
+}
+
 int main() {
+  /*
+  Salvaremos os tempos em variáveis individuais afim de
+  melhorar a vizualização da tabela final
+  */ 
+  //Auxiliares p/ cálculo dos tempos
+  int iniTime, endTime;
+  //Tempos bubble sort
+  int bubbleT1, 
+      bubbleT5, 
+      bubbleT10, 
+      bubbleT15, 
+      bubbleT20, 
+      bubbleT30, 
+      bubbleT40;
+  //Tempos selection sort
+
+
   //Valida a criação dos vetores com os valores do arquivo p/ prosseguir
   if(createArrays()){
-    
+    //IMPLEMENTAÇÕES BUBBLE SORT
+    //n = 1000
+    cout << "Bubble Sort 1k" << endl;
+    iniTime = (int) clock();
+    bubbleSort(vet1, V1LEN);
+    endTime = (int) clock();
+    bubbleT1 = ((endTime - iniTime)*1000/CLOCKS_PER_SEC);
+    //n = 5000
+    cout << "Bubble Sort 5k" << endl;
+    iniTime = (int) clock();
+    bubbleSort(vet5, V5LEN);
+    endTime = (int) clock();
+    bubbleT5 = ((endTime - iniTime)*1000/CLOCKS_PER_SEC);
+    //n = 10000
+    cout << "Bubble Sort 10k" << endl;
+    iniTime = (int) clock();
+    bubbleSort(vet10, V10LEN);
+    endTime = (int) clock();
+    bubbleT10 = ((endTime - iniTime)*1000/CLOCKS_PER_SEC);
+    //n = 15000
+    cout << "Bubble Sort 15k" << endl;
+    iniTime = (int) clock();
+    bubbleSort(vet15, V15LEN);
+    endTime = (int) clock();
+    bubbleT15 = ((endTime - iniTime)*1000/CLOCKS_PER_SEC);
+    //n = 20000
+    cout << "Bubble Sort 20k" << endl;
+    iniTime = (int) clock();
+    bubbleSort(vet20, V20LEN);
+    endTime = (int) clock();
+    bubbleT20 = ((endTime - iniTime)*1000/CLOCKS_PER_SEC);
+    //n = 30000
+    cout << "Bubble Sort 30k" << endl;
+    iniTime = (int) clock();
+    bubbleSort(vet30, V30LEN);
+    endTime = (int) clock();
+    bubbleT30 = ((endTime - iniTime)*1000/CLOCKS_PER_SEC);
+    //n = 40000
+    cout << "Bubble Sort 40k" << endl;
+    iniTime = (int) clock();
+    bubbleSort(vet40, V40LEN);
+    endTime = (int) clock();
+    bubbleT40 = ((endTime - iniTime)*1000/CLOCKS_PER_SEC);
+    //Impressão da tabela parcial
+    cout << "\nn\tBubble\t" << endl;
+    cout << "1000\t" << bubbleT1 << endl;
+    cout << "5000\t" << bubbleT5 << endl;
+    cout << "10000\t" << bubbleT10 << endl;
+    cout << "15000\t" << bubbleT15 << endl;
+    cout << "20000\t" << bubbleT20 << endl;
+    cout << "30000\t" << bubbleT30 << endl;
+    cout << "40000\t" << bubbleT40 << endl;
+
+    //IMPLEMENTAÇÕES SELECTION SORT
+    //...
+
+    //Impressão da tabela final
+    cout << "\nTabela Final\n" << endl;
+    cout << "\nn\tBubble\tSelection" << endl;
+    cout << "1000\t" << bubbleT1 << endl;
+    cout << "5000\t" << bubbleT5 << endl;
+    cout << "10000\t" << bubbleT10 << endl;
+    cout << "15000\t" << bubbleT15 << endl;
+    cout << "20000\t" << bubbleT20 << endl;
+    cout << "30000\t" << bubbleT30 << endl;
+    cout << "40000\t" << bubbleT40 << endl;
   } else {
     cout << "Erro ao abrir arquivo..." << endl;
   }
